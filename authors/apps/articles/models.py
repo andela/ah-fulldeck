4b6d68b7c2234eac81d1ec9f7815fd55eb4c6871
Hesbon <hesbonkiptoo5600@gmail.com>
2019-01-20 21:26:56 +0300

    feature(articles crud): user should be able to perform the following
    - create an article
    - view one article
    - view all articles
    - delete an article
    - update an article
    
    [Delivers #162948994]
    
    feature (Password Reset): Reset password through email
     - Confirms user email if it exists
     - Generates token for the user using email provided
     - Sends verification link to the user email
     - Asks user to enter and confirm password
     - On success, response is given to the user
    [Delivers #162948991]
    
    feature(profile): users should be able to create their profiles
    
    - create profile
    - retrieve profile
    
    [starts #162948957]

---
+++ authors/apps/articles/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.utils.text import slugify
+
+from authors.apps.authentication.models import User
+
+
+class Article(models.Model):
+    """
+    The model for creating the articles is defined here
+    """
+    slug = models.SlugField(
+        db_index=True, max_length=1000, unique=True, blank=True)
+    title = models.CharField(max_length=1000, blank=False)
+    description = models.CharField(max_length=2000, blank=False)
+    body = models.TextField(blank=False)
+    image_url = models.URLField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    author = models.ForeignKey(
+        User, related_name='article', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def create_slug(self):
+        """
+        Before the title is saved it has to be converted to a slug
+        'This is a title' will be converted to This-is-a-title
+        """
+        slug = slugify(self.title)
+        new_slug = slug
+        n = 1
+        while Article.objects.filter(slug=new_slug).exists():
+            new_slug = '{}-{}'.format(slug, n)
+            n += 1
+
+        return new_slug
+
+    def save(self, *args, **kwargs):
+        """
+        Ensure that before an article is saved,
+        it must have a slug created from the title
+        """
+        if not self.slug:
+            self.slug = self.create_slug()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
